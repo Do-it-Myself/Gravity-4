126faaef80b425f0d50a50c0c9ea191fda032245
tyamcy <100736625+tyamcy@users.noreply.github.com>
2023-01-20 16:40:17 +0000

    description complete

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,8 +3,24 @@ import './Home.css';
 
 function Home(props) {
     return (
-        <div className='test'>
-            <h1>Gravity 4</h1>
+        <div className='home'>
+            <div className='game-info'>
+                <h2>Gravity 4</h2>
+                <p>A twist on the classic Connect 4</p>
+            </div>
+
+            <div className='player-turn'>
+                <h4>{props.player} turn</h4>
+            </div>
+            
+            
+            <div className=''>
+
+            </div>
+
+            <div className='play-button'>
+
+            </div>
         </div>
     );
 }
