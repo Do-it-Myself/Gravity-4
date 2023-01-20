2bfe1b25b01e8193a46e0b9dc781acb96318b6fc
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-01-20 16:45:51 +0000

    unfinished table

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Table from "../../components/table/Table"
 
 function Home(props) {
     return (
         <div>
             <h1>Gravity 4</h1>
+            <Table />
         </div>
     );
 }
