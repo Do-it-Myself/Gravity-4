4419bb89334d5527656018dc122b9b91f1a9826e
benkw26 <bensonxkwok@gmail.com>
2023-01-20 17:40:25 +0000

    button

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,31 +1,30 @@
-import React from "react";
-import "./Home.css";
-import Button from "../../components/button/Button";
-import Table from "../../components/table/Table";
+import React from 'react';
+import './Home.css';
+import Button from '../../components/button/Button';
+import Table from '../../components/table/Table';
 
 function Home(props) {
-  return (
-    <div className="home">
-      <div className="game-info">
-        <h2>Gravity 4</h2>
-        <p>A twist on the classic Connect 4</p>
-      </div>
+    return (
+        <div className='home'>
+            <div className='game-info'>
+                <h2>Gravity 4</h2>
+                <p>A twist on the classic Connect 4</p>
+            </div>
 
-      <div className="player-turn">
-        <h4>{props.player} turn</h4>
-      </div>
+            <div className='player-turn'>
+                <h4>{props.player} turn</h4>
+            </div>
+            
+            
+            <div className='flip-button'>
 
-      <div className="flip-button">
-        <div className="">
-          <Table />
+            </div>
+
+            <div className='play-button'>
+                <Button />
+            </div>
         </div>
-
-        <div className="play-button">
-          <Button />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
