93d5fe3a001abca2f6754b032f9bd6a00d2547e7
tyamcy <100736625+tyamcy@users.noreply.github.com>
2023-01-21 10:34:43 +0000

    end modal

--- src/components/endmodal/Endmodal.js
+++ src/components/endmodal/Endmodal.js
@@ -1,5 +1,5 @@
 import {React, useState} from 'react';
-import './Instructions.css';
+import './Endmodal.css';
 
 function Endmodal(props) {
     const[closeModal, setCloseModal] = useState(true);
@@ -7,20 +7,11 @@ function Endmodal(props) {
     return (
         <div>
             {closeModal && (
-                <div className='instructions-popup'>
-                    <button onClick={() => setCloseModal(false)} className='close-button'>
-                        <IoCloseSharp size={24}/>
-                    </button>
-                    <div className='instructions-text'>
-                        <h3>How to play?</h3>
-                        <div className='description'>
-                            <ul>
-                                <li>Same as Connect 4, the objective is to connect 4 tokens either horizontally, vertically, or diagonally.</li>
-                                <li>But in Gravity 4, you can either place a token or flip the board for your turn.</li>
-                                <li>Enjoy the game and experience a whole new dimension!</li>
-                            </ul>
-                        </div>
+                <div className='end-modal'>
+                    <div className='winner'>
+                        <h3>{props.winner} Wins!</h3>
                     </div>
+                    <button onClick={() => setCloseModal(false)} className='play-again-button'>Play again</button>
                 </div>
             )}
         </div>
