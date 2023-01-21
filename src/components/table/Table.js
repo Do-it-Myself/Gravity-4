da05a101e7d0b275c31edada4c7ac022c746acbf
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-01-21 11:35:51 +0000

    simplify

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -155,94 +155,84 @@ function Table() {
   }
 
   function flip() {
-    for (let c = 0; c < 7; c++) {
-      const column = state.board.map((x) => x[c]);
-      let lastFilled = 7;
-      for (let r = 0; r < 7; r++) {
-        if (column[r] !== 0) {
-          lastFilled = r;
-          break;
+    if (state.flipped) {
+      for (let c = 0; c < 7; c++) {
+        const column = state.board.map((x) => x[c]);
+        let lastFilled = -1;
+        for (let r = 6; r >= 0; r--) {
+          if (column[r] !== 0) {
+            lastFilled = r;
+            break;
+          }
         }
+        if (lastFilled !== -1) {
+          let newColumn = [0, 0, 0, 0, 0, 0, 0];
+          const shift = 6 - lastFilled;
+          for (let r = 6; r >= shift; r--) {
+            newColumn[r] = column[r - shift];
+          }
+          for (let r = 0; r < 7; r++) {
+            state.board[r][c] = newColumn[r];
+          }
+        }
+        state.flipped = false;
       }
-      if (lastFilled !== 7) {
-        let newColumn = [0, 0, 0, 0, 0, 0, 0];
-        for (let r = 0; r < 7 - lastFilled; r++) {
-          newColumn[r] = column[r + lastFilled];
-        }
+    } else {
+      for (let c = 0; c < 7; c++) {
+        const column = state.board.map((x) => x[c]);
+        let lastFilled = 7;
         for (let r = 0; r < 7; r++) {
-          state.board[r][c] = newColumn[r];
+          if (column[r] !== 0) {
+            lastFilled = r;
+            break;
+          }
+        }
+        if (lastFilled !== 7) {
+          let newColumn = [0, 0, 0, 0, 0, 0, 0];
+          for (let r = 0; r < 7 - lastFilled; r++) {
+            newColumn[r] = column[r + lastFilled];
+          }
+          for (let r = 0; r < 7; r++) {
+            state.board[r][c] = newColumn[r];
+          }
         }
       }
+      state.flipped = true;
     }
-    state.flipped = true;
     setState({ ...state });
     checkWinner(state.board);
   }
 
-  function unflip() {
-    for (let c = 0; c < 7; c++) {
-      const column = state.board.map((x) => x[c]);
-      let lastFilled = -1;
-      for (let r = 6; r >= 0; r--) {
-        if (column[r] !== 0) {
-          lastFilled = r;
+  function playHandler(rowIndex, columnIndex) {
+    console.log("Table", rowIndex, columnIndex);
+    const column = state.board.map((x) => x[columnIndex]);
+    if (state.flipped) {
+      for (let i = 0; i < 7; i++) {
+        if (column[i] === 0) {
+          if (state.currentPlayerIs1) {
+            state.board[i][columnIndex] = 1;
+          } else {
+            state.board[i][columnIndex] = 2;
+          }
+          state.currentPlayerIs1 = !state.currentPlayerIs1;
+          setState({ ...state });
           break;
         }
       }
-      if (lastFilled !== -1) {
-        let newColumn = [0, 0, 0, 0, 0, 0, 0];
-        const shift = 6 - lastFilled;
-        for (let r = 6; r >= shift; r--) {
-          newColumn[r] = column[r - shift];
-        }
-        for (let r = 0; r < 7; r++) {
-          state.board[r][c] = newColumn[r];
+    } else {
+      for (let i = 6; i >= 0; i--) {
+        if (column[i] === 0) {
+          if (state.currentPlayerIs1) {
+            state.board[i][columnIndex] = 1;
+          } else {
+            state.board[i][columnIndex] = 2;
+          }
+          state.currentPlayerIs1 = !state.currentPlayerIs1;
+          setState({ ...state });
+          break;
         }
       }
-      state.flipped = false;
-      setState({ ...state });
-      checkWinner(state.board);
     }
-  }
-
-  function playHandler(rowIndex, columnIndex) {
-    console.log("Table", rowIndex, columnIndex);
-
-    const column = state.board.map((x) => x[columnIndex]);
-    for (let i = 6; i >= 0; i--) {
-      if (column[i] === 0) {
-        if (state.currentPlayerIs1) {
-          state.board[i][columnIndex] = 1;
-        } else {
-          state.board[i][columnIndex] = 2;
-        }
-        state.currentPlayerIs1 = !state.currentPlayerIs1;
-        setState({ ...state });
-        break;
-      }
-    }
-
-    console.log(state.board, state.currentPlayerIs1);
-    checkWinner(state.board);
-  }
-
-  function playHandlerFlipped(rowIndex, columnIndex) {
-    console.log("Table", rowIndex, columnIndex);
-
-    const column = state.board.map((x) => x[columnIndex]);
-    for (let i = 0; i < 7; i++) {
-      if (column[i] === 0) {
-        if (state.currentPlayerIs1) {
-          state.board[i][columnIndex] = 1;
-        } else {
-          state.board[i][columnIndex] = 2;
-        }
-        state.currentPlayerIs1 = !state.currentPlayerIs1;
-        setState({ ...state });
-        break;
-      }
-    }
-
     console.log(state.board, state.currentPlayerIs1);
     checkWinner(state.board);
   }
@@ -253,7 +243,7 @@ function Table() {
         key={i}
         row={row}
         rowIndex={i}
-        playHandler={state.flipped ? playHandlerFlipped : playHandler}
+        playHandler={playHandler}
       />
     );
   }
@@ -263,7 +253,7 @@ function Table() {
       <table>
         <tbody>{state.board.map(RowMap)}</tbody>
       </table>
-      <button onClick={state.flipped ? unflip : flip}>Flip</button>
+      <button onClick={flip}>Flip</button>
     </GameContext.Provider>
   );
 }
